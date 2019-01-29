31e08a67a315e110a7982d881f200fb5900ba95a
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-01-29 13:54:13 +0200

    Create Pb10.scala

---
+++ PbSheet2/src/main/scala/Pb10.scala
@@ -0,0 +1,15 @@
+
+object pb10{
+
+def eval(a: Array[Double], x: Double) : Double =
+ {
+   val n=a.size
+   var sum:Double=0
+   while(n>0){
+      //n>=0
+      n=n-1
+    	sum=sum*x+a(n)
+     }
+   sum 
+ }
+}
