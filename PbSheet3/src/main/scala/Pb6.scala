d503223c312b2293d6c1578db5fe7a467d5ef17d
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-05 15:20:21 +0200

    Create Pb6.scala

---
+++ PbSheet3/src/main/scala/Pb6.scala
@@ -0,0 +1,24 @@
+package solutions
+
+object pb6{
+
+def partition2(l: Int, r: Int) : Int = {
+    val x = a(l) // pivot
+    // Invariant a[l+1..i) < x = a(l) <= a[j..r) && l < i <= j <= r
+    // && a(j) < x && a[0..l) = a_0[0..l) && a[r..N) = a_0[r..N)
+    // && a[l..r) is a permutation of a_0[l..r)
+    var i = l+1; var j = r
+    
+    while(a(j-1) >= x && i < j)j -= 1
+
+    while(i < j){
+      if(a(i) < x) i += 1
+      else{
+        val t = a(i); a(i) = a(j-1); a(j-1) = t; j -= 1
+      }
+    }
+    // swap pivot into position
+    a(l) = a(i-1); a(i-1) = x
+    i-1 // position of the pivot
+  }
+}
