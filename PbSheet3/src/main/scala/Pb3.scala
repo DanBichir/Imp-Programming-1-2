37cc4b0e7c70cd7495ede4defb9f248cafe8fe0e
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-05 14:33:06 +0200

    Create Pb3.scala

---
+++ PbSheet3/src/main/scala/Pb3.scala
@@ -0,0 +1,35 @@
+package solutions
+
+
+object pb3{
+  def tooBig(y: BigInt, x: BigInt): Boolean = {
+      y > x
+  }
+//a 10 calls
+  def search1(x: BigInt): BigInt = {
+    var i=0
+    var j=1000
+    while(i<j){
+      if(tooBig(i+(j-i)/2,x)) i = i+(j-i)/2
+      else j = i+(j-i)/2
+    }
+    i
+  }
+  // b
+  def search2(x: BigInt): BigInt = {
+    var i = 1
+    while(tooBig(i,x)==0)  i=i*2
+    var j = i
+    i = i/2
+    while(i<j){
+      if(tooBig(i+(j-i)/2,x)) i = i+(j-i)/2
+      else j = i+(j-i)/2
+    }
+    i
+  }
+  // We change i=i*2 to i=i*(2^(1/eps))
+      
+  
+
+
+}
