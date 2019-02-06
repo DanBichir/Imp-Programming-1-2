c3c3b62b4da4f012021adf687a89420d6b7a4431
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-06 11:43:30 +0200

    Update Pb2.scala

--- PbSheet3/src/main/scala/Pb2.scala
+++ PbSheet3/src/main/scala/Pb2.scala
@@ -1,8 +1,38 @@
 package solutions
 
 object pb2{
+  def sqrt(y:Int) : Int = {
+    assert(y>=0)
+    var a = 0; var b = y+1
+    while(a+1<b){
+      val m = a + (b-a)/2
+      if(m*m<=y) a = m 
+      else       b = m
+    }
+    return a
+  }
   def search(a: Int) : Int = {
   var i = 0; var j = a
+  var x = (j-i)/3
+  var m = i+x
+  var n = i+x*2
+  while(j>i){
+    if (m*m==a) return m
+    if(m*m>a)   j = m
+    else if(n*n<a) i = n
+    else if (n*n ==a) return n
+    else{
+      i = m+1
+      j = n
+    }
+    x = (j-i)/3
+    m = i+x
+    n = i+x*2
+  }
+  i
+}
+  def maxsearch(a: Int) : Int = {
+  var i = 0; var j = 46000
   var x = (j-i)/3
   var m = i+x
   var n = i+x*2
