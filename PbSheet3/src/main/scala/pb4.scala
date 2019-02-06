b715f0ae2c10594b1a33b5bd3cc64226cbc7d39a
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-06 12:15:18 +0200

    Update and rename Pb4.scala to pb4.scala

--- PbSheet3/src/main/scala/pb4.scala
+++ PbSheet3/src/main/scala/pb4.scala
@@ -1,17 +1,17 @@
 package solutions
 
-object Pb4{
-def sort(a : Array[Int]) : Array[Int] = {
+object pb4{
+def sort(A : Array[Int]) : Array[Int] = {
   for(j<- 0 to A.length – 1){ 
      key = A(j)
-      i = j
+     i = j
       while (i > 0 && A(i – 1) > key){ 
          A(i) = A(i – 1)
-          i = i − 1
+         i = i − 1
       }
       A(i) = key
     }
-    a
+    A
   }
 }
 
