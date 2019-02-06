6c2b3676b56036745c2c1ecd7bb690af751a7a52
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-06 17:11:30 +0200

    Update pb3.scala

--- PbSheet3/src/main/scala/pb3.scala
+++ PbSheet3/src/main/scala/pb3.scala
@@ -5,13 +5,14 @@ object pb3{
   def tooBig(y: BigInt, x: BigInt): Boolean = {
       y > x
   }
-//a) 10 calls
+  //a) 10 calls
   def search1(x: BigInt): BigInt = {
     var i=0
     var j=1000
     while(i<j){
-      if(tooBig(i+(j-i)/2,x)) i = i+(j-i)/2
+      if(tooBig(i+(j-i)/2,x)) i = i+(j-i)/2 
       else j = i+(j-i)/2
+      //halves the interval with every step (like binary search)
     }
     i
   }
@@ -21,6 +22,7 @@ object pb3{
     while(tooBig(i,x)==0)  i=i*2
     var j = i
     i = i/2
+    // searches in interval [i,j]
     while(i<j){
       if(tooBig(i+(j-i)/2,x)) i = i+(j-i)/2
       else j = i+(j-i)/2
@@ -28,8 +30,4 @@ object pb3{
     i
   }
   //c) We change i=i*2 to i=i*(2^(1/eps))
-      
-  
-
-
-}
+ }
