b054f662ce1603ffdd678bc6cc6947cf33c3cad6
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-12 12:54:26 +0200

    Update pb2.scala

--- PbSheet4/src/main/scala/pb2.scala
+++ PbSheet4/src/main/scala/pb2.scala
@@ -1,1 +1,20 @@
 
+
+package solutions
+
+trait Stack[A] {
+  
+  
+  /** Adds new element to the stack
+    * post: S = x ++ S */
+  def push(x : A)
+
+  /** Removes newest element from the stack
+    * pre: !isEmpty()
+    * post: S = tail S ^ return head S */
+  def pop() : A
+
+  /** Checks if empty
+    * post: S = S ^ return S == [] */
+  def isEmpty()
+}
