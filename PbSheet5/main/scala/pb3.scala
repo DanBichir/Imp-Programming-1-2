b25f4b3678748629a552cbd08e91c22e578b3d9a
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-27 07:24:24 +0200

    Create pb3.scala

---
+++ PbSheet5/main/scala/pb3.scala
@@ -0,0 +1,65 @@
+package solutions
+
+object linkedList{
+  private class Node(val name:String, var number:String, var next:Node)
+}
+
+object pb3{
+    class linkedList extends Book{
+        private var list : linkedList.Node =  new linkedList.Node("?","?",null)
+
+        /* Find the node with the name */
+        private def findpre(name:String) : linkedList.Node = {
+            var n = list
+            while(n.next != null && n.next.name <= name) n = n.next
+            return n
+        }
+
+        private def find(name:String) : linkedList.Node = {
+            findpre(name).next
+        }
+
+        /* Find number based on name */
+        def recall(name: String) : String = {
+            val n = find(name)
+            assert(n!=null)
+            return n.number
+        }
+
+        /* Add the name and number to the mapping */
+        def store(name: String, number: String) = {
+            val n = find(name)
+            if(n.name != name){
+              var prev = findpre(name)
+              prev.next = new linkedList.Node(name, number, prev.next)
+            }
+            else n.number = number
+        }
+        
+         /* Check for name */
+        def check(name: String): Boolean = find(name) != null
+
+        /* Remove name from mapping */
+        def remove(name: String) = {
+            if(check(name)){
+              var n = findpre(name)
+              n.next = n.next.next
+            }
+        }
+    }
+}
+
+/*
+Function abstraction:
+
+state s: seq Int
+s = list(head.next)
+list(null) = []
+list(a) = a.datum:L(a.next)
+
+Datatype Invariants : list(head.next) is finite, the names in list(head.next) are distinct and list(head.next) is sorted
+
+
+
+
+*/
