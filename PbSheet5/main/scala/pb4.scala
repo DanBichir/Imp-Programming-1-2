07dbd64b1ba2285274308f95b3c0d8ffd1862b7a
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-27 07:56:04 +0200

    Create pb4.scala

---
+++ PbSheet5/main/scala/pb4.scala
@@ -0,0 +1,62 @@
+package solutions
+
+/*
+a)
+
+E(X)=sum(p(i)*(i*c)) + q*(n*c)
+We have the sequences p(i) that we can rearrange. 
+The mininum of our expectation is achieved when we pair the greatest values of p(i) with the smallest values of i*c and smallest with greatest.
+Assume p(i)<p(j) for i<j, then p(i)*i*c + p(j)*j*c > p(j)*i*c + p(i)*j*c => p(i)>p(j) for i<j
+
+*/
+
+//b)
+
+object pb4{
+    object linkedList{
+        private class Node(val name:String, var number:String, var next:Node)
+    }
+      
+    class linkedList extends Book{
+        private var list : linkedList.Node =  new linkedList.Node("?","?",null)
+
+        /* Find the node with the name */
+        private def findpre(name:String) : linkedList.Node = {
+            var n = list
+            while(n.next != null && n.next.name <= name) n = n.next
+            return n
+        }
+
+        private def find(name:String) : linkedList.Node = {
+            findpre(name).next
+        }
+
+        /* Find number based on name */
+        def recall(name: String) : String = {
+            val n = find(name)
+            assert(n!=null)
+            return n.number
+        }
+
+        /* Add the name and number to the mapping */
+        def store(name: String, number: String) = {
+            val n = find(name)
+            if(n.name != name){
+              var prev = findpre(name)
+              prev.next = new linkedList.Node(name, number, prev.next)
+            }
+            else n.number = number
+        }
+        
+         /* Check for name */
+        def check(name: String): Boolean = find(name) != null
+
+        /* Remove name from mapping */
+        def remove(name: String) = {
+            if(check(name)){
+              var n = findpre(name)
+              n.next = n.next.next
+            }
+        }
+    }
+}
