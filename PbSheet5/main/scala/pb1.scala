256edf96bcd363750147a4862001713420e5f728
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-27 06:16:37 +0200

    Update pb1.scala

--- PbSheet5/main/scala/pb1.scala
+++ PbSheet5/main/scala/pb1.scala
@@ -13,38 +13,33 @@ class Node(var datum: Int, var next: Node){
     }
 }
 
-
 object pb1{
-  def createList(){
-    var myList : Node = null
-    i=1
-    while(i<=12){
-      myList = new Node(i,myList)
-      i+=1
-    }
-    println("The new list is " + myList)
-  }
-
-  def reverseList(){
-    var myList : Node = null
-    i=0
-    while(i<=12){
-      myList = new Node(i,myList)
-      i+=1
-    }
-    println("Initial list is " + myList)
-    var reversed : Node = null
-    var k : Node = null
-
-    //Invariant : reversed = reverse takes i 
-    while(myList != null){
-      k = myList.next
-      myList.next = reversed
-      reversed = myList
-      myList = k
+    def createList(){
+        var myList : Node = null
+        i=1
+        while(i<=12){
+            myList = new Node(i,myList)
+            i+=1
+        }
+        println("The new list is " + myList)
     }
 
-    println("Reversed list is " + revList)
-
-  }
+    def reverseList(){
+        var myList : Node = null
+        i=1
+        while(i<=12){
+            myList = new Node(i,myList)
+            i+=1
+        }
+        println("Initial list is " + myList)
+        var reversed : Node = null
+        var k : Node = null
+        while(myList != null){
+            k = myList.next
+            myList.next = reversed
+            reversed = myList
+            myList = k
+        }
+        println("Reversed list is " + revList)
+    }
 }
