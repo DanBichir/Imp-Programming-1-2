6c00cd3dc573d8ea57fe79468f9c94b0bd1e9f64
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-03-06 09:37:09 +0000

    Add files via upload

---
+++ Pb1.scala
@@ -0,0 +1,11 @@
+package solutions
+/*
+
+Binary Search Tree is an alternative with lower asymptotic complexity of O(log N)
+
+It can be slower in other processes such as adding or removing a new key.
+If we just want to use it for searching, then it should be more efficient than Linked Lists.
+Another advantage is that the method organises the data.
+
+
+*/
