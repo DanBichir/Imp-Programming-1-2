afb250f6f594d90dc0073cbb2d1e3b092c840fbf
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-05-14 19:20:16 +0300

    Update Pb7.scala

--- ImpPro3/PbSheet2/Pb7.scala
+++ ImpPro3/PbSheet2/Pb7.scala
@@ -1,1 +1,11 @@
+/*
 
+Issues: n*n may overflow
+        It is slow for big tests
+        It should take only positive int values
+
+Possible solution:  We want to check sqrt(n)*sqrt(n) == n (solves the overflow problem and it may be faster)
+                    Because sqrt(n) is irrational, it will always be an approximation of the actual result.
+                    so we check if n.toFloat - sqrt(n)*sqrt(n) < e, where e is a fixed sufficiently small value
+
+*/
